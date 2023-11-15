7a3f859e61afae74383f232d4690301b922ba2cc
MJ <douob55@gmail.com>
2023-11-16 08:32:45 +0900

    20231116오전

--- wasRepo/webPrj09/src/main/java/com/kh/app/home/HomeServlet.java
+++ wasRepo/webPrj09/src/main/java/com/kh/app/home/HomeServlet.java
@@ -3,7 +3,6 @@ package com.kh.app.home;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,14 +14,16 @@ public class HomeServlet extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		//문자열 내보내기
+
 //		resp.setContentType("text/html; charset=UTF-8;");
 //		PrintWriter out = resp.getWriter();
-//		out.write("<h1>홈페이지</h1>");
+//		out.write("<h1>home~~~!!</h1>");
+		
+//		resp.getWriter().write("<h1>home~~~!!</h1>");
 		
 		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
 		
+	
 	}
 	
 }
--- wasRepo/webPrj11/src/main/java/com/kh/app/home/HomeServlet.java
+++ wasRepo/webPrj11/src/main/java/com/kh/app/home/HomeServlet.java
@@ -1,26 +1,19 @@
 package com.kh.app.home;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"/" , "/home" , "/home/*"})  // "/" 로 작성하거나 "/home" , "/home/뒤에 어떤 문자열이 와도 다 포함"으로 요청해도 다 잘 나온다.
 public class HomeServlet extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
-		//문자열 내보내기
-//		resp.setContentType("text/html; charset=UTF-8;");
-//		PrintWriter out = resp.getWriter();
-//		out.write("<h1>홈페이지</h1>");
-		
 		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
 		
 	}
